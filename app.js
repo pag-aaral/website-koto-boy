b02c666e43429429f2c4c85181fcf5bc239965f8
pag-aaral <theblackdragonyuzhong@gmail.com>
2026-04-26 11:27:50 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let db = JSON.parse(localStorage.getItem("db")) || {
+schedule:[],
+messages:[]
+};
+
+function save(){
+localStorage.setItem("db",JSON.stringify(db));
+localStorage.setItem("update",Date.now());
+}
+
+/* REALTIME SYNC */
+setInterval(()=>{
+db = JSON.parse(localStorage.getItem("db")) || {schedule:[],messages:[]};
+if(typeof renderAdmin==="function") renderAdmin();
+if(typeof renderStudent==="function") renderStudent();
+},800);
